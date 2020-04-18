0a1ae459b7e59c0c3c63593987e1131c3abf0d78
Mehul9999 <62761247+Mehul9999@users.noreply.github.com>
2020-04-18 23:23:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, send_from_directory, render_template
+from flask import request, redirect, flash
+
 app = Flask(__name__)
 
+app.secret_key = 'key'
+db = {'admin': '123456'}
 
-@app.route('/')
-def index():
-       return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def login_user():
+    global s
+    try:
+        if db[request.form['user']] == request.form['pwd']:
+            s = True
+            return redirect('cabinet')
+    except:
+        msg = "not post request"
+    return render_template('login.html')
 
+@app.route('/cabinet')
+def cabinet():
+    if s == True:
+        return render_template('index.html')
+    else:
+        return redirect('/')
 
-@app.route('/img/<path:path>')
-def index2(path):
-       return send_from_directory('static/img',path)
-       
-@app.route('/static/<path:path>')
-def index3(path):
-     return app.send_static_file(path)
-        
-if __name__ == '__main__':
+@app.route('/img/<path:filename>')
+def imgFile(filename):
+    return send_from_directory('/static/img', filename)
 
-    #app.run(host='127.0.0.1',port='5000',debug = True,use_reloader=False)
-    app.run(threaded=True, port='5000')
+@app.route('/static/css/<string:stylesheet>')
+def css(stylesheet):
+    return send_from_directory('static/css', stylesheet)
+
+if __name__ == "__main__":
+    app.run(debug=True
